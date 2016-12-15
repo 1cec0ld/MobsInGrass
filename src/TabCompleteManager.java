a28aeaf014f1ad9fab2afecf7096bc99fa7b139a
Jeremy Luchak <jeremy@nevada.unr.edu>
2016-12-15 19:44:57 +0000

    TabCompleteManager.java created online with Bitbucket

---
+++ src/TabCompleteManager.java
@@ -0,0 +1,87 @@
+package com.gmail.ak1cec0ld.plugins.MobsInGrass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+public class TabCompleteManager implements TabCompleter{
+
+    private MobsInGrass plugin;
+    
+    public TabCompleteManager(MobsInGrass plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (command.getName().equalsIgnoreCase("mig")){
+            List<String> suggestions = new ArrayList<>();
+            if (args.length == 1){
+                suggestions.add("search");
+                suggestions.add("add");
+                suggestions.add("remove");
+                suggestions.add("reload");
+                suggestions.add("help");
+            } else if (args.length == 2){
+                if (StringUtils.containsIgnoreCase(args[0], "search")){
+                    for (String c : plugin.configManager.getConfiguredMaterials()){
+                        if (!suggestions.contains(c)){
+                            suggestions.add(c);
+                        }
+                    }
+                    for (Material v : Material.values()){
+                        for (String stri : plugin.configManager.getMobsForThisMaterial(v.name())){
+                            if (!suggestions.contains(stri) && !stri.contains("can spawn")){
+                                suggestions.add(stri);
+                            }
+                        }
+                    }
+                } else if (args[0].equalsIgnoreCase("remove")){
+                    for (String c : plugin.configManager.getConfiguredMaterials()){
+                        if (!suggestions.contains(c)){
+                            suggestions.add(c);
+                        }
+                    }
+                } else if (args[0].equalsIgnoreCase("add")){
+                    suggestions.add("AIR");
+                    suggestions.add("SAPLING");
+                    suggestions.add("FLOWING_WATER");
+                    suggestions.add("WATER");
+                    suggestions.add("FLOWING_LAVA");
+                    suggestions.add("LAVA");
+                    suggestions.add("GOLDEN_RAIL");
+                    suggestions.add("DETECTOR_RAIL");
+                    suggestions.add("WEB");
+                    suggestions.add("LONG_GRASS");
+                    suggestions.add("DOUBLE_PLANT");
+                    suggestions.add("DEAD_BUSH");
+                    suggestions.add("RED_ROSE");
+                    suggestions.add("YELLOW_FLOWER");
+                    suggestions.add("BROWN_MUSHOOM");
+                    suggestions.add("RED_MUSHROOM");
+                    suggestions.add("STONE_SLAB");
+                    suggestions.add("TORCH");
+                    suggestions.add("FIRE");
+                    suggestions.add("WOOD_STAIRS");
+                    suggestions.add("REDSTONE_WIRE");
+                    suggestions.add("STANDING_SIGN");
+                    suggestions.add("STONE_PRESSURE_PLATE");
+                    suggestions.add("WOODEN_PRESSURE_PLATE");
+                    suggestions.add("REEDS");
+                }
+            }
+            if (suggestions.isEmpty()){
+                return null;
+            } else {
+                return suggestions;
+            }
+        }
+        return null;
+    }
+
+}
